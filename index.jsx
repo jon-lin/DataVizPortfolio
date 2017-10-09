23bef9369ca1d348c34b7ad1abc2cdc4c52830cf
Jonathan Lin <jclin2013@gmail.com>
2017-10-09 00:07:26 -0400

    Get React framework setup

---
+++ index.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<div>Hello</div>, root);
+});
