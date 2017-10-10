71c8a8a485f8e75e95c79429a14eacbf956c3766
Jonathan Lin <jclin2013@gmail.com>
2017-10-10 10:54:07 -0400

    Add rotating cube to animation banner

--- components/AnimationBanner.jsx
+++ components/AnimationBanner.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 class AnimationBanner extends React.Component {
   render() {
     return (
-      <div id="animationBanner">I'm an animation</div>
+      <div id="animationBanner">
+        <div className="cubeWrap">
+  			  <div className="cube">
+  			    <div className="front">Github</div>
+  			    <div className="back">LinkedIn</div>
+  			    <div className="top">AngelList</div>
+  			    <div className="bottom">Website</div>
+  			    <div className="left">Resume</div>
+  			    <div className="right">Email</div>
+  			  </div>
+  			</div>
+			</div>
     )
   }
 }
